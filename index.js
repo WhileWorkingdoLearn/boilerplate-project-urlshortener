859815fcdd490e25391294fcc8ebb8a2b61f01b7
s_ms_on_github <90606181+WhileWorkingdoLearn@users.noreply.github.com>
2024-08-24 22:39:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const port = process.env.PORT || 3000;
 
 const parser = bodyParser.urlencoded({extended:false});
 
+const urlMap = new Map();
+
 app.use(parser);
 
 app.use(cors());
@@ -20,28 +22,33 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-const urlMap = new Map();
 
 app.post('/api/shorturl', function(req, res) {  
   const url = req.body.url;
+  console.log(url);
   const urlRegexPattern = /^https?:\/\/(www\.)?[\w-]+\.\w{2,5}/;
   if(!urlRegexPattern.test(url)){
     res.json({error: 'invalid url'});
   }else{
     const shortUrlNumber = Math.floor(Math.random() * 10000).toString();
     urlMap.set(shortUrlNumber, url);
+    console.log("shorUrlNumber" +shortUrlNumber);
     res.json({original_url: url, short_url: shortUrlNumber});
   }
 });
 
+
 app.get('/api/shorturl/:shortUrl', function(req, res) {
   const shortUrl = req.params.shortUrl;
+  console.log(shortUrl);
+  console.log(urlMap.has(shortUrl));
   if(urlMap.has(shortUrl)){
     const url = urlMap.get(shortUrl);
     res.redirect(url);} else {
       res.json({error: 'invalid url'});
     }
 });
+
 
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
